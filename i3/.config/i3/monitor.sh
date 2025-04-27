d8754420ab0abba235e4210d9d2f67043625b66d
PaulsyWaulsy <paul.chemonges@hotmail.com>
2025-04-27 12:42:17 +1000

    desktop

--- i3/.config/i3/monitor.sh
+++ i3/.config/i3/monitor.sh
@@ -5,3 +5,9 @@ if xrandr | grep "HDMI-2 connected"; then
 else
     xrandr --output eDP-1 --auto
 fi
+
+if xrandr | grep "DP-2 connected"; then
+    xrandr --output DP-2 --auto --right-of DP-0
+else
+    xrandr --output DP-0 --auto
+fi
